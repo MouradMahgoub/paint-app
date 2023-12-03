c76aea9cee5f76bab44e31276c17f6f742264bdc
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2023-12-03 21:14:35 +0200

    m

--- backend/src/main/java/com/youssif/mourad/paint/app/paint/shapes/Circle.java
+++ backend/src/main/java/com/youssif/mourad/paint/app/paint/shapes/Circle.java
@@ -2,8 +2,8 @@ package com.youssif.mourad.paint.app.paint.shapes;
 
 public class Circle extends Shape{
 
-    Circle(int id, int x, int y, String fill) {
-        super(id, x, y, fill);
+    Circle(String type, int id, int x, int y, String fill) {
+        super(type, id, x, y, fill);
         //TODO Auto-generated constructor stub
     }
     
