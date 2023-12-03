a4cf6287e2d39fb2940651be4f7c0b6b16c7e7c7
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2023-12-03 03:36:00 +0200

    test

--- backend/src/main/java/com/youssif/mourad/paint/app/PaintAppApplication.java
+++ backend/src/main/java/com/youssif/mourad/paint/app/PaintAppApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaintAppApplication {
-//testcf
-//testcdffdcfjdckjf
 	public static void main(String[] args) {
 		SpringApplication.run(PaintAppApplication.class, args);
 	}
