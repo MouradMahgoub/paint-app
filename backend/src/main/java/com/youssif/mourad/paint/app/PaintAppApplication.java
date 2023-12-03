a233279c7f8a3d7b0664dfc771f8dc59b3643e43
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2023-12-03 03:00:44 +0200

    first commit

---
+++ backend/src/main/java/com/youssif/mourad/paint/app/PaintAppApplication.java
@@ -0,0 +1,14 @@
+package com.youssif.mourad.paint.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintAppApplication {
+//testcf
+//testcdffdcfjdckjf
+	public static void main(String[] args) {
+		SpringApplication.run(PaintAppApplication.class, args);
+	}
+
+}
