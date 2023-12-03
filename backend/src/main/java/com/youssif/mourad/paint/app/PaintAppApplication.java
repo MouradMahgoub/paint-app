915723629b1c988234e50d7c60334c9eb12b90d8
elkarnk <youssef.alaa.ahmed.haredy@gmail.com>
2023-12-03 10:07:52 +0200

    now

--- backend/src/main/java/com/youssif/mourad/paint/app/PaintAppApplication.java
+++ backend/src/main/java/com/youssif/mourad/paint/app/PaintAppApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaintAppApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(PaintAppApplication.class, args);
+
+		SpringApplication.run(PaintAppApplication.class, args
+				// boom
+		);
 	}
 
 }
